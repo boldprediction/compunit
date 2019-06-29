1e1bf60b578bc0fa70e2d91c940bf2e16108e958
Li Zhang <zhangli.cmu@gmail.com>
2019-06-28 21:22:01 -0400

    added semantic model and loader, create a meta-singleton class

---
+++ src/hubs/semanticmodels.py
@@ -0,0 +1,33 @@
+import os
+
+from hubs.config import Config
+from constant import SEMANTICMODELS_DIR
+from utils.singleton import MetaSingleton
+from models.semanticmodel import SemanticModel
+
+
+class SemanticModels(metaclass=MetaSingleton):
+
+    class Singleton:
+
+        def __init__(self):
+            self.infos = Config.semanticmodels
+            self.models = {}
+
+        def __getattr__(self, key):
+            if key == "infos":
+                return self.infos
+            elif key == "models":
+                return self.models
+            else:
+                # no corresponding semantic model
+                if key not in self.infos:
+                    raise AttributeError("No corresponding semantic model: "+key)
+
+                # load model
+                if key not in self.models:
+                    path = os.path.join(SEMANTICMODELS_DIR, self.infos[key])
+                    self.models[key] = SemanticModel.load(path)
+
+                return self.models[key]
+
--- src/main.py
+++ src/main.py
@@ -7,19 +7,16 @@ from hubs.logger import Logger
 
 import time
 
+from hubs.semanticmodels import SemanticModels
+
 
 class Main:
 
     def start(self):
-        # self.config_cortex()
-
-        begin = time.time()
-        array = Subjects.english1000
-        print(time.time()- begin)
+        # subjects = Subjects.english1000
+        print(SemanticModels.english1000)
 
 
-    # def config_cortex(self):
-    #     print(config.get('basic', 'filestore'))
 
 
 if __name__ == "__main__":
