79aacabf39048a9dc8448d0b1f0e437887dd8fc1
Li Zhang <zhangli.cmu@gmail.com>
2019-07-01 18:24:48 -0400

    add more dependencies

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 python3 -m venv venv/
 source venv/bin/activate
+pip3 install decorator==4.4.0       
+pip3 install future==0.17.1 
+pip3 install html5lib==1.0.1 
+pip3 install networkx==2.1
+pip3 install scipy==1.2.0
+pip3 install setuptools==41.0.1
+pip3 install Shapely==1.6.4.post2
+pip3 install six==1.12.0
+pip3 install webencodings==0.5.1
+pip3 install wheel==0.33.4
+
 pip3 install tables==3.5.1
 pip3 install nibabel==2.4.0
 pip3 install Cython==0.29.6
