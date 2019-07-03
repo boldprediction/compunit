961cdf6ea5fcdc93d8c1e9d5fdccc0558e4ffc69
Weiwei Huang <weiweihu@andrew.cmu.edu>
2019-07-03 00:43:35 -0400

    separate core parallel

--- core/parallelize.py
+++ core/parallelize.py
@@ -1,21 +1,199 @@
 import os
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager,Process,Queue, Pipe
+import time
+
+
+# def pool_paral_func(param):
+#     subject,contrast,do_pmap = param[0],param[1],param[2]
+#     # paral_func using queue
+#     print("start one process")
+#     begin = time.time()
+#     res = subject.run(contrast, do_pmap)
+#     print("[process calculation time cost] "+str(time.time()-begin))
+#     print("finished one process")
+#     return res
+
+# def pool_async_paral_func(subject,contrast,do_pmap):
+#     # paral_func using queue
+#     print("start one process")
+#     begin = time.time()
+#     res = subject.run(contrast, do_pmap)
+#     print("[process calculation time cost] "+str(time.time()-begin))
+#     print("finished one process")
+#     return res
+
+# def parallelize(execs):
+
+#     res = []
+#     tokens = []
+#     pro_count = min(len(execs), os.cpu_count()-1)
+
+#     # shared memory
+#     manager = Manager()
+#     shared_list = manager.list()
+
+#     with Pool(processes = pro_count) as pool:
+
+#         for exec in execs:
+#             l = len(exec)
+#             func, param, kwparam = exec[0], exec[1] if l > 1 else [], exec[2] if l > 2 else {}
+#             param.append(shared_list)
+#             token = pool.apply_async(func,param,kwparam)
+#             tokens.append(token)
+
+#         for t in tokens:
+#             res.append(t.get())
+
+#     return res
+
+# def parallelize(execs):
+#     # parallelize using manager managed shared memory 
+
+#     begin = time.time()
+#     print("start parallelizing")
+
+#     res = []
+#     tokens = []
+#     pro_count = min(len(execs), os.cpu_count()-1)
+
+#     # shared memory
+#     manager = Manager()
+#     l_exec = len(execs)
+#     shared_list = manager.list([0]*l_exec)
+#     p_list= []
+#     for i in range(l_exec):
+#         exec = execs[i]
+#         l = len(exec)
+#         func,param , kwparam = exec[0], exec[1] if l > 1 else [], exec[2] if l > 2 else {}
+#         param.append(shared_list)
+#         param.append(i)
+#         p_list.append(Process(target=func, args=param))
+
+#     for p in p_list:
+#         p.start()
+#     for p in p_list:
+#         p.join()
+    
+#     print("[parallelizing time cost] "+str(time.time()-begin))
+
+#     print("result = ", shared_list)
+#     return shared_list
+
+
+# def parallelize(execs):
+#     # parallelize using q = Queue()
+
+#     begin = time.time()
+#     print("start parallelizing")
+
+#     res = []
+#     tokens = []
+#     pro_count = min(len(execs), os.cpu_count()-1)
+
+#     # shared memory
+#     l_exec = len(execs)
+#     p_list= []
+#     q = Queue()
+#     for exec in execs:
+#         l = len(exec)
+#         func,param , kwparam = exec[0], exec[1] if l > 1 else [], exec[2] if l > 2 else {}
+#         param.append(q)
+#         p_list.append(Process(target=func, args=param))
+
+#     for p in p_list:
+#         p.start()
+#     for p in p_list:
+#         p.join()
+#         print("one process joined ")
+
+    
+#     print("[parallelizing time cost] "+str(time.time()-begin))
+
+#     print(q)
+#     return []
+
+
+# def parallelize(execs):
+#     # parallelize using Pool
+
+#     begin = time.time()
+#     print("start parallelizing")
+
+#     res = []
+#     params_list = []
+
+#     for exec in execs:
+#         l = len(exec)
+#         func, param , kwparam = exec[0], exec[1] if l > 1 else [], exec[2] if l > 2 else {}
+#         params_list.append(param)
+
+#     with Pool(3) as pool:
+#         res.append(pool.map(pool_paral_func,params_list))
+    
+#     print("[parallelizing time cost] "+str(time.time()-begin))
+#     print("res = ", res)
+#     return res
+
+# def parallelize(execs):
+#     # parallelize using Pool async
+#     res = []
+#     tokens = []
+#     pro_count = min(len(execs), os.cpu_count()-1)
+
+#     begin = time.time()
+#     print("start parallelizing")
+
+#     with Pool(processes = pro_count) as pool:
+
+#         for exec in execs:
+#             l = len(exec)
+#             func, param, kwparam = exec[0], exec[1] if l > 1 else [], exec[2] if l > 2 else {}
+#             token = pool.apply_async(pool_async_paral_func,param,kwparam)
+#             tokens.append(token)
+
+#         for t in tokens:
+#             res.append(t.get())
+    
+#     print("[parallelizing time cost] "+str(time.time()-begin))
+#     print("res = ", res)
+
+#     return res
+
 
 def parallelize(execs):
+    # parallel using pipe -  This is the fastest !!!
+
+    begin = time.time()
+    print("start parallelizing")
 
     res = []
     tokens = []
-    pro_count = min(len(execs), os.cpu_count()-1)
 
-    with Pool(processes = pro_count) as pool:
+    l_exec = len(execs)
+    p_list= []
 
-        for exec in execs:
-            l = len(exec)
-            func, param, kwparam = exec[0], exec[1] if l > 1 else [], exec[2] if l > 2 else {}
-            token = pool.apply_async(func,param,kwparam)
-            tokens.append(token)
+    readers = []
 
-        for t in tokens:
-            res.append(t.get())
+    for exec in execs:
+        r, w = Pipe(duplex=False)
+        readers.append(r)
+        l = len(exec)
+        func,param , kwparam = exec[0], exec[1] if l > 1 else [], exec[2] if l > 2 else {}
+        param.append(w)
+        p = Process(target=func, args=param)
+        p.start()
+
+    while readers:
+        for r in readers:
+            try:
+                msg = r.recv()
+                res.append(msg)
+            except EOFError:
+                readers.remove(r)
+            else:
+                print(msg)
+                readers.remove(r)
+
+    print("[parallelizing time cost] "+str(time.time()-begin))
 
     return res
--- core/subject_group.py
+++ core/subject_group.py
@@ -1,6 +1,60 @@
 import time
 from parallelize import parallelize
 
+# def paral_func(subject,contrast,do_pmap,shared_list):
+#     # paral_func using shared list
+#     print("start one process")
+#     begin = time.time()
+#     res = subject.run(contrast, do_pmap)
+#     print("[process calculation time cost] "+str(time.time()-begin))
+
+#     print("start one process writing to shared list")
+#     begin = time.time()
+#     shared_list.append(res)
+#     print("[process writing time cost] "+str(time.time()-begin))
+#     print("finished one task")
+#     return  0
+
+# def paral_func(subject,contrast,do_pmap,que):
+#     # paral_func using queue
+#     print("start one process")
+#     begin = time.time()
+#     res = subject.run(contrast, do_pmap)
+#     print("result = ",res)
+#     print("[process calculation time cost] "+str(time.time()-begin))
+
+#     print("start one process writing to queue")
+#     begin = time.time()
+#     que.put(res)
+#     print("[process writing time cost] "+str(time.time()-begin))
+#     print("finished one process")
+#     return  0
+
+def paral_func(subject,contrast,do_pmap,writer):
+    # paral_func using pipe
+    print("start one process")
+    begin = time.time()
+    res = subject.run(contrast, do_pmap)
+    # print("result = ",res)
+    print("[process calculation time cost] "+str(time.time()-begin))
+
+    print("start one process writing to pipe")
+    begin = time.time()
+    writer.send(res)
+    print("[process writing time cost] "+str(time.time()-begin))
+    print("finished one process")
+    writer.close()
+    return  0
+
+# def paral_func(subject,contrast,do_pmap,shared_list,index):
+#     # paral_func using  independent shared list
+#     print("start one process")
+#     begin = time.time()
+#     shared_list[index] = subject.run(contrast, do_pmap)
+#     print("[process calculation + writing time cost] "+str(time.time()-begin))
+#     print("finished one task")
+#     return  0
+
 class SubjectGroup:
 
     def __init__(self, subjects, analyses, do_pmap = False):
@@ -34,10 +88,10 @@ class SubjectGroup:
         
         begin = time.time()
 
-        # executions = [(sub.run, [contrast, do_pmap]) for sub in self.subjects]
-        # records = parallelize(executions)
+        executions = [(paral_func, [sub,contrast, do_pmap]) for sub in self.subjects]
+        records = parallelize(executions)
 
-        records = [sub.run(contrast, do_pmap = do_pmap) for sub in self.subjects]
+        # records = [sub.run(contrast, do_pmap = do_pmap) for sub in self.subjects]
 
         print("[time cost] "+str(time.time()-begin))
 
@@ -49,3 +103,8 @@ class SubjectGroup:
 
         # Combine and return
         return [groupres] + output
+
+
+
+## python3.8 3.9 supports a shared memory direct access 
+## https://docs.python.org/3.8/library/multiprocessing.shared_memory.html
