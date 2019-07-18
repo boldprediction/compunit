6ef92ac7d5ddedaa31118210eeb614f787ec405b
Li Zhang <zhangli.cmu@gmail.com>
2019-07-17 21:52:42 -0400

    trying to use make_static_light

--- src/analysis/group/webglgroup.py
+++ src/analysis/group/webglgroup.py
@@ -4,6 +4,8 @@ import numpy as np
 from utils import clsname
 from serializer import Serializable
 from analysis.group import GroupAnalysis
+from utils.cortex import make_static_light
+
 
 class WebGLGroup(GroupAnalysis):
 
@@ -28,6 +30,8 @@ class WebGLGroup(GroupAnalysis):
             res_dict = {'contrast': mean_volume, 'pmap': mean_perm_volume}
 
         # jsonstr = cortex.webgl.make_static_light(self.tmp_image_dir, res_dict)
-        result = {'images': {'__42f1b009d46afdc8': ['/static/simulate/data/__42f1b009d46afdc8_0.png']}, 'data': {'__42f1b009d46afdc8': {'name': '__42f1b009d46afdc8', 'min': -2.258884205267101, 'max': 2.543631070038396, 'raw': False, 'shape': [182, 218, 182], 'mosaic': [15, 11], 'subject': 'MNI'}}, 'views': [{'xfm': [[-1, 0, 0, 90, 0, 1, 0, 126, 0, 0, 1, 72, 0, 0, 0, 1]], 'name': 'contrast', 'vmin': [-2], 'state': None, 'cmap': ['RdBu_r'], 'attrs': {'priority': 1}, 'vmax': [2], 'data': ['__42f1b009d46afdc8'], 'desc': ''}]}
+        # result = {'images': {'__42f1b009d46afdc8': ['/static/simulate/data/__42f1b009d46afdc8_0.png']}, 'data': {'__42f1b009d46afdc8': {'name': '__42f1b009d46afdc8', 'min': -2.258884205267101, 'max': 2.543631070038396, 'raw': False, 'shape': [182, 218, 182], 'mosaic': [15, 11], 'subject': 'MNI'}}, 'views': [{'xfm': [[-1, 0, 0, 90, 0, 1, 0, 126, 0, 0, 1, 72, 0, 0, 0, 1]], 'name': 'contrast', 'vmin': [-2], 'state': None, 'cmap': ['RdBu_r'], 'attrs': {'priority': 1}, 'vmax': [2], 'data': ['__42f1b009d46afdc8'], 'desc': ''}]}
+
+        result = make_static_light(res_dict)
 
         return Serializable({clsname(self): json.dumps(result)})
